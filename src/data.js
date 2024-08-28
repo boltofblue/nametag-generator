c8ab22a44477d17584063b64d7d283a512b33394
jycbrwndev <jayci.brown1@gmail.com>
2024-08-28 14:30:48 -0700

    Build a data model

---
+++ src/data.js
@@ -0,0 +1,9 @@
+const nameData = [
+    { id: 1, name: "Jayci" },
+    { id: 2, name: "Embie" },
+    { id: 3, name: "Squish" },
+    { id: 4, name: "Togepi" },
+    { id: 5, name: "Pikachu" }
+  ];
+
+  export default nameData;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag";
+import nameData from "./data";
 
 import "./styles.css";
 
-const App = () => (
-  <div className="App">
-    <h1>Name Tag Generator</h1>
-    <NameTag />
-  </div>
-);
+const renderNameTag = (nameObject) => {
+  return <NameTag key={nameObject.id} name={nameObject.name} />;
+};
+
+const App = () => {
+  const NameTagElements = nameData.map(renderNameTag);
+  return (
+    <div className="App">
+      <h1>Name Tag Generator</h1>
+      {NameTagElements} {/* render the NameTagElements variable */}
+    </div>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
