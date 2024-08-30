0fe71d0ea71dc4d4add433e6262361a6279f5778
jycbrwndev <jayci.brown1@gmail.com>
2024-08-30 12:27:59 -0700

    Make generate nametag interactive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import NameTag from "./NameTag";
+import NameTagList from "./NameTagList";
+import UserInput from "./UserInput";
 import "./styles.css";
 
 class App extends Component {
   state = {
     names: ["Embie", "Togepi", "Squish", "Jayci", "Cyndaquil"]
   };
-
+addName = (name) => {
+    const newNames = [name, ...this.state.names];
+    this.setState({ names: newNames });
+};
 removeName = (clickedIndex) => {
     const filterCallback = (_, index) => index !== clickedIndex;
     const newNames = this.state.names.filter(filterCallback);
@@ -16,7 +20,7 @@ removeName = (clickedIndex) => {
     return (
       <div className="App">
         <h1>Name Tag Generator</h1>
-        <UserInput />
+        <UserInput addName={this.addName} />
         <NameTagList names={this.state.names} removeName={this.removeName} />
       </div>
     );
